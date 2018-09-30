1c13043e45e6659f85b01e1880cf81c1579cc9aa
loktevalexey <loktev.alex.74@gmail.com>
2018-09-30 22:09:02 +0300

    первый пример упрощён

--- A_TS0-TS1/TS005_start/app.ts
+++ A_TS0-TS1/TS005_start/app.ts
@@ -1,21 +1,3 @@
-
-var elem=this.document.getElementById("content");
-
-class Car {
-
-   num:string;
-   price:number;
-
-   constructor(_num:string, _price:number) {
-       this.num=_num;
-       this.price=_price;
-   }
-
-   getInfo():string {
-       return 'car num='+this.num+' price='+this.price;
-   }
-
-}
-
-var car1:Car=new Car("2870 ОГО",1200);
-elem.innerHTML="Информация о машинке: " + car1.getInfo();
+var n:number=5;
+var s:string="hello ";
+console.log(s+n);
